c606590141579132c000ef20c17897997a686d38
GS04-38-yukifujita <huji0327@gmail.com>
2016-05-26 23:18:17 +0900

    no message

---
+++ 9/ang.js
@@ -0,0 +1,8 @@
+var mainCtrl = function($scope){
+    $scope.users = [
+        {'name':'fujita', 'score':53.33},
+        {'name':'taguchi', 'score':74.43},
+        {'name':'tanaka', 'score':34.35}
+    ];
+    $scope.today = new Date();
+}
